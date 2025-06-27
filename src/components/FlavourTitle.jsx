6628eac82b39de3a0b3d3a695ce41a606fc926ff
Sankalp Tiwari <sankalp20tiwari@gmail.com>
2025-06-27 17:09:35 +0530

    fix

--- src/components/FlavourTitle.jsx
+++ src/components/FlavourTitle.jsx
@@ -47,7 +47,7 @@ const FlavourTitle = () => {
 
 
   return (
-    <div className='general-title col-center h-full 2xl:gap-32 xl:gap-24 gap-16'>
+    <div className='general-title col-center h-full 2xl:gap-32 xl:gap-24 gap-12 sm:gap-16'>
         <div className="overflow-hidden 2xl:py-0 py-3 first-text-split">
             <h1>
                 We have 6 
