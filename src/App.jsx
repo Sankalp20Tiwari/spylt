b1575ec951f151d6b5051acc905ec4b0abb739de
Sankalp Tiwari <sankalp20tiwari@gmail.com>
2025-06-28 17:53:27 +0530

    final app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,44 @@
-import React from 'react'
+import Navbar from './components/Navbar'
+import HeroSection from './sections/HeroSection'
+import { ScrollSmoother, ScrollTrigger } from 'gsap/all'
+import gsap from 'gsap'
+import MessageSection from './sections/MessageSection'
+import FlavourSection from './sections/FlavourSection'
+import { useGSAP } from '@gsap/react'
+import NutritionSection from './sections/NutritionSection'
+import BenefitSection from './sections/BenefitSection'
+import TestimonialSection from './sections/TestimonialSection'
+import FooterSection from './sections/FooterSection'
+
+gsap.registerPlugin(ScrollTrigger , ScrollSmoother)
 
 const App = () => {
+
+  useGSAP(()=>{
+    ScrollSmoother.create({
+      smooth:3,
+      effects:true,
+    })
+  })
+
   return (
-    <div>
-      <h1 className='text-3xl font-bold  mt-10'>
-        Hello, Vite + React + Tailwind CSS!
-      </h1>
-    </div>
+    <main>
+      <Navbar />
+      <div id="smooth-wrapper">
+      <div id="smooth-content">
+        <HeroSection />
+        <MessageSection />
+        <FlavourSection />
+        <NutritionSection />
+        <div>
+        <BenefitSection />
+        <TestimonialSection />
+        </div>
+
+        <FooterSection />
+      </div>
+      </div>
+    </main>
   )
 }
 
